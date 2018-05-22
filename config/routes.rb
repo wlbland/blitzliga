69d9c0a3b339983ad8ca93e700785f58cc6c4e2d
William Bland <williamlbland@yahoo.co.uk>
2018-05-22 14:39:00 +0100

    Prepare for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,8 @@ Rails.application.routes.draw do
   resources :fixtures, only: [:show, :index]
 
    get '/future', to: 'fixtures#future', as: 'future'
+
+   get "/pages/:page" => "pages#show"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
