551717b0da9177bb4efb7fc9fc87b5cb1f8469d5
William Bland <williamlbland@yahoo.co.uk>
2018-06-02 07:38:19 +0100

    catains started

--- db/seeds/captains.rb
+++ db/seeds/captains.rb
@@ -1,2 +1,7 @@
-coalition_captain = Player.where(last_name: "Balmforth")
-coalition_captain.update(captain: true)
+
+galactics = Team.find_by(name: "The Galactics")
+vukosi = Player.create(first_name: "Vukosi", last_name: "Kazamula", team: galactics)
+galactics_captain = vukosi
+
+uns_captain = Player.where("last_name = ? AND first_name = ?", "Chapman", "Jack")
+uns_captain.update(captain: true)
