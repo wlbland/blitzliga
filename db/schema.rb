006c8eb9ee505485ffd22ef5037aed0675ed4e12
William Bland <williambland@Williams-MacBook-Pro.local>
2018-10-02 17:24:33 +0100

    hide inactive teams on teams page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181002161428) do
+ActiveRecord::Schema.define(version: 20181002162320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,7 +104,6 @@ ActiveRecord::Schema.define(version: 20181002161428) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "display"
-    t.boolean "bl_display"
   end
 
   create_table "users", force: :cascade do |t|
